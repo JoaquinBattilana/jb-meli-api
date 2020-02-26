dd5d624a778e7ceb0f20bb309ed35c535a76431b
JoaquinBattilana <battilanajn@gmail.com>
2020-02-26 19:20:37 -0300

    added basic eslint configuration

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const express = require('express');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('Hello world');
-});
+app.get('/', (req, res) => res.send('Hello world'));
 
 app.listen(process.env.PORT || 3000);
