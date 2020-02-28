de4f7c62e319ca1f6b50e1c6799e4bee2c1a345d
JoaquinBattilana <battilanajn@gmail.com>
2020-02-28 05:38:43 -0300

    hotfixed cors problem

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+};
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use('/api/items', require('./api/routes/items'));
 
 module.exports = app;
